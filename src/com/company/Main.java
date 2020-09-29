061b3992852c0312ed93f3b3def81801ba67caea
filipsupolik25@gmail.com <14Filip14>
2020-09-29 09:28:47 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter how many inputs you want: ");
+        int b=input.nextInt();
+        int[] a = new int[b];
+
+        for (int i = 0; i < a.length; i++)
+        {
+            System.out.println("Please enter number");
+            a[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]*a.length);
+        }
+    }
+}
